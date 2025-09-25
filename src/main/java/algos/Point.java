7d2133aa206868ec22fb4c8c9a31480027325289
Tayalov <241752@astanait.edu.kz>
2025-09-25 16:05:15 +0500

    Update Point.java

--- src/main/java/algos/Point.java
+++ src/main/java/algos/Point.java
@@ -1,12 +1,6 @@
 package algos;
 
 public class Point {
-    public final double x;
-    public final double y;
+    public double x, y;
     public Point(double x, double y) { this.x = x; this.y = y; }
-
-    public double distance(Point other) {
-        double dx = x - other.x, dy = y - other.y;
-        return Math.hypot(dx, dy);
-    }
 }
